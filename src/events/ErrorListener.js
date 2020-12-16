9ab9aaca8ff6371b220e531e287288aad62c54a3
DanielaGC <danielagc676@gmail.com>
2020-12-16 15:39:54 -0300

    Update ErrorListener

--- src/events/ErrorListener.js
+++ src/events/ErrorListener.js
@@ -7,7 +7,7 @@ module.exports = class ErrorListener extends EventListener {
 
     run(client, err, shardID) {
         Logger.error(`An error has been ocorred in shard ${shardID}: ${err?.message}`)
-        if (client.shards.get(shardID).status === 'offline') {
+        if (client.shards.get(shardID).status === 'disconnected') {
             client.shards.get(shardID).connect()
         }
     }
